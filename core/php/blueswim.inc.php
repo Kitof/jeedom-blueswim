9cdd065370ae63a19fc92074d4ddfe2a87f7c76f
Kitof <github@kitof.net>
2021-05-06 17:52:39 +0200

    Add logs

--- core/php/blueswim.inc.php
+++ core/php/blueswim.inc.php
@@ -15,7 +15,7 @@
 * You should have received a copy of the GNU General Public License
 * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
 */
-
+require_once dirname(__FILE__) . '/logging.inc.php';
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 require_once dirname(__FILE__) . '/../../vendor/autoload.php';
 /*
